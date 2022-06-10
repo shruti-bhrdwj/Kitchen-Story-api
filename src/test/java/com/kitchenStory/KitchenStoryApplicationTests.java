7e1f3faa7aef70e97e8ca9e0f7930aa12e35896e
Shruti <bhardwaj.shruti03@gmail.com>
2022-06-10 19:15:13 +0530

    version 1.0

--- src/test/java/com/kitchenStory/KitchenStoryApplicationTests.java
+++ src/test/java/com/kitchenStory/KitchenStoryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dell.webservice;
+package com.kitchenStory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaFsdPhase4AssessmentEcommerceKitchenStoryApplicationTests {
+class KitchenStoryApplicationTests {
 
 	@Test
 	void contextLoads() {
